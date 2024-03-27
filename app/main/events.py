5eb1e3931b64bdb55783ae4e9b478ba861fe9f84
AleDV <alessandro.divanni@gmail.com>
2024-03-27 22:00:55 +0100

    test loop

--- app/main/events.py
+++ app/main/events.py
@@ -1,7 +1,36 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
+import threading
+import time
 
+loop_running = False
+
+def loop_function(room):
+    global loop_running
+    i = 0
+    while loop_running:
+        i = i + 1
+        # Esegui il codice del tuo loop qui
+        # Ad esempio, invia dati al frontend tramite socket.emit()
+        emit('status', {'msg': session.get('name') + ' loop ' + str(i)}, room=room)
+        #socketio.emit('update', {'data': 'Nuovi dati dal backend'})
+        time.sleep(1)
+
+@socketio.on('start_loop', namespace='/chat')
+def start_loop():
+    global loop_running
+    if not loop_running:
+        loop_running = True
+        room = session.get('room')
+        join_room(room)
+        # Avvia il loop in un thread separato
+        threading.Thread(target=loop_function(room)).start()
+
+@socketio.on('stop_loop', namespace='/chat')
+def stop_loop():
+    global loop_running
+    loop_running = False
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
